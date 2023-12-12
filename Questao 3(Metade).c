b0c0e0c2c9b7668b82ef70ccf9424a0d0a7c299b
Bruno252 <143636176+Bruno252@users.noreply.github.com>
2023-12-12 08:24:39 -0300

    Atualizar o Questao 3.c

--- Questao 3(Metade).c
+++ Questao 3(Metade).c
@@ -1,35 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 50
 
-struct dados{
-char nome[250];
-float preco;
-int qnt;
+struct dados {
+    char nome[250];
+    float preco;
+    float valorQnt;
+    int qnt;
+    int qntTotal;
 };
 
-float valorTotal(struct dados produto[]){
-
-
-
+void limpatudo(){
+system("cls || clear");
+fflush(stdin);
+//setbuf(stdin,NULL);
 }
 
+void a() {
+    printf("=================================\n");
+}
 
-int main(){
-struct dados produto[50];
-int escolha;
-int i=0;
+float valorSoma(int contador,struct dados produto[]) {
+    int i=contador;
+       return produto[i].preco*produto[i].qnt;
+    
+}
 
-printf("1 - Realizar uma compra\n");
-printf("2 - Consultar estoque\n");
-printf("3 - Sair do programa.\n");
-scanf("%d",&escolha); 
+int subt(int contador,struct dados produto[],int qnt) {
+    int i=contador;
+    return produto[i].qntTotal-=qnt;
+}
 
-do{
+float valorTotal(int contador,struct dados produto[]) {
+    float soma=0;
+    int i;
+    for(i=0; i<contador; i++) {
+        soma+=produto[i].valorQnt;
+    }
+    return soma;
+}
 
-    switch(escolha){
+void imprimir(int contador,struct dados produto[],float soma) {
+    int i;
+    for(i=0; i<contador; i++) {
+        a();
+        printf("Nome do Produto:%s",produto[i].nome);
+        printf("Valor do Produto:%.2f\n",produto[i].preco);
+        printf("Quantidade retirada do estoque:%d\n",produto[i].qnt);
+        printf("Valor por Quantidade:%.2f\n",produto[i].valorQnt);
+        a();
+    }
+    printf("Valor Total:%.2f",soma);
+}
+
+void imprimirEstoque(int contador,struct dados produto[]) {
+    int i;
+    for(i=0; i<contador; i++) {
+        printf("Nome do Produto:%s",produto[i].nome);
+        printf("Quantidade no estoque:%d\n",produto[i].qntTotal);
+        printf("\n");
+    }
+}
+
+int main() {
+    struct dados produto[TAM];
+    int escolha;
+    float soma=0;
+    int i=0;
+
+    do {
+        a();
+        printf("1 - Realizar uma compra\n");
+        printf("2 - Consultar estoque\n");
+        printf("3 - Sair do programa.\n");
+        a();
+        scanf("%d",&escolha);
+        switch(escolha) {
 
         case 1:
-            printf("Nome:");
+            produto[i].qntTotal=25;
+            fflush(stdin);
+            setbuf(stdin,NULL);
+            printf("\nNome:");
             fgets(produto[i].nome,sizeof(produto[i].nome),stdin);
 
             printf("Preco:");
@@ -37,25 +89,37 @@ do{
 
             printf("Quantidade:");
             scanf("%d",&produto[i].qnt);
-            valorTotal(produto);
-            
-            i+1;
-        break;
+
+            produto[i].qntTotal=subt(i,produto,produto[i].qnt);
+            produto[i].valorQnt=valorSoma(i,produto);
+
+            i++;
+            limpatudo();
+            break;
 
         case 2:
-            i+1;
-        break;
+            a();
+            imprimirEstoque(i,produto);
+            a();
+            break;
 
-    
-        break;
+            break;
+
+        case 3:
+
+            break;
 
         default:
             printf("Escolha Invalida");
 
-        break;
+            break;
 
-    }
+        }
 
-}while(escolha!=3);
+    } while(escolha!=3);
+    limpatudo();
+    soma=valorTotal(i,produto);
+    imprimir(i,produto,soma);
+
     return 0;
 }
